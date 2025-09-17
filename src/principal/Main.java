a509679792a523b6323d5835139245c492273015
Crossxb <fabryleguizamon43@gmail.com>
2025-09-17 13:03:58 -0300

    Añadi un facherisimo hola mundo

--- src/principal/Main.java
+++ src/principal/Main.java
@@ -5,6 +5,11 @@ import gestor.AplicacionTransporte;
 public class Main {
 
     public static void main(String[] args) {
+        
+        
+        System.out.println("Hola mundo aguante boca");
+        
+        
         AplicacionTransporte app = new AplicacionTransporte();
         app.ejecutarAplicacion();
         
